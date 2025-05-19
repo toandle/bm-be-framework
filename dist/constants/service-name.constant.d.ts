de4105ce82dac0aed4300af08ee213d4eb395424
Toan Le <ldtoan.dev@gmail.com>
2025-05-19 09:14:10 +0700

    include dist folder

---
+++ dist/constants/service-name.constant.d.ts
@@ -0,0 +1,4 @@
+export declare const AUTH_SERVICE = "auth-service";
+export declare const RESOURCE_SERVICE = "resource-service";
+export declare const BUSINESS_SERVICE = "business-service";
+export declare const API_GATEWAY = "api-gateway";
---
+++ dist/constants/service-name.constant.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.API_GATEWAY = exports.BUSINESS_SERVICE = exports.RESOURCE_SERVICE = exports.AUTH_SERVICE = void 0;
+exports.AUTH_SERVICE = 'auth-service';
+exports.RESOURCE_SERVICE = 'resource-service';
+exports.BUSINESS_SERVICE = 'business-service';
+exports.API_GATEWAY = 'api-gateway';
+//# sourceMappingURL=service-name.constant.js.map
