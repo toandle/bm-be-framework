4057b68a86970e90170b17511213f97253873a20
Toan Le <ldtoan.dev@gmail.com>
2025-05-19 09:02:32 +0700

    init

---
+++ src/constants/service-name.constant.ts
@@ -0,0 +1,4 @@
+export const AUTH_SERVICE = 'auth-service';
+export const RESOURCE_SERVICE = 'resource-service';
+export const BUSINESS_SERVICE = 'business-service';
+export const API_GATEWAY = 'api-gateway';
