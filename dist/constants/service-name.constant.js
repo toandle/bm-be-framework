9e2f11fe3afc2c1fe7037d537b1ee8844734f590
Toan Le <ldtoan.dev@gmail.com>
2025-05-20 15:35:36 +0700

    run compile

--- dist/constants/service-name.constant.js
+++ dist/constants/service-name.constant.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ADMIN_SERVICE = exports.AUDIT_SERVICE = exports.SEARCH_SERVICE = exports.KYC_SERVICE = exports.BANKING_SERVICE = exports.INVOICE_SERVICE = exports.BOOKING_SERVICE = exports.NOTIFICATION_SERVICE = exports.BUSINESS_SERVICE = exports.RESOURCE_SERVICE = exports.AUTH_SERVICE = void 0;
-exports.AUTH_SERVICE = 'auth-service';
-exports.RESOURCE_SERVICE = 'resource-service';
-exports.BUSINESS_SERVICE = 'business-service';
-exports.NOTIFICATION_SERVICE = 'notification-service';
-exports.BOOKING_SERVICE = 'booking-service';
-exports.INVOICE_SERVICE = 'invoice-service';
-exports.BANKING_SERVICE = 'banking-service';
-exports.KYC_SERVICE = 'kyc-service';
-exports.SEARCH_SERVICE = 'search-service';
-exports.AUDIT_SERVICE = 'audit-service';
-exports.ADMIN_SERVICE = 'admin-service';
+exports.BM_ADMIN_SERVICE = exports.BM_AUDIT_SERVICE = exports.BM_SEARCH_SERVICE = exports.BM_KYC_SERVICE = exports.BM_BANKING_SERVICE = exports.BM_INVOICE_SERVICE = exports.BM_BOOKING_SERVICE = exports.BM_NOTIFICATION_SERVICE = exports.BM_BUSINESS_SERVICE = exports.BM_RESOURCE_SERVICE = exports.BM_AUTH_SERVICE = void 0;
+exports.BM_AUTH_SERVICE = 'bm-auth-service';
+exports.BM_RESOURCE_SERVICE = 'bm-resource-service';
+exports.BM_BUSINESS_SERVICE = 'bm-business-service';
+exports.BM_NOTIFICATION_SERVICE = 'bm-notification-service';
+exports.BM_BOOKING_SERVICE = 'bm-booking-service';
+exports.BM_INVOICE_SERVICE = 'bm-invoice-service';
+exports.BM_BANKING_SERVICE = 'bm-banking-service';
+exports.BM_KYC_SERVICE = 'bm-kyc-service';
+exports.BM_SEARCH_SERVICE = 'bm-search-service';
+exports.BM_AUDIT_SERVICE = 'bm-audit-service';
+exports.BM_ADMIN_SERVICE = 'bm-admin-service';
 //# sourceMappingURL=service-name.constant.js.map
